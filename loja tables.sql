cfccaef3162d96f6094c9904946f1d17b57e3b6e
Yami <mu.meiraaz@gmail.com>
2020-12-02 04:33:58 -0300

    Update customer table, add sales_items table

--- loja tables.sql
+++ loja tables.sql
@@ -14,7 +14,7 @@ name	VARCHAR(20) NOT NULL,
 
 DROP TABLE brand
 CREATE TABLE brand(
-id		INT IDENTITY(10,1) PRIMARY KEY,
+id		INT IDENTITY(1,1) PRIMARY KEY,
 name	VARCHAR(20) NOT NULL,
 )
 
@@ -30,7 +30,7 @@ name VARCHAR(20)  PRIMARY KEY
 
 DROP TABLE product 
 CREATE TABLE product(
-id			INT IDENTITY(100,1) PRIMARY KEY,
+id			INT IDENTITY(1,1) PRIMARY KEY,
 name		VARCHAR(100) NOT NULL,
 description	VARCHAR(200),
 price		DECIMAL(9,2) NOT NULL,
@@ -57,10 +57,12 @@ PRIMARY KEY (product, color)
 
 DROP TABLE customer 
 CREATE TABLE customer(
-cpf		CHAR(11) PRIMARY KEY,
-name	VARCHAR(100) NOT NULL,
-uf		CHAR(2) NOT NULL,
-cep		CHAR(8) NOT NULL
+cpf			CHAR(11) PRIMARY KEY,
+name		VARCHAR(100) NOT NULL,
+uf			CHAR(2) NOT NULL,
+cep			CHAR(8) NOT NULL,
+email		VARCHAR(50) NOT NULL,
+password	VARCHAR(20) NOT NULL
 )
 
 DROP TABLE card
@@ -73,12 +75,25 @@ FOREIGN KEY (customer) REFERENCES customer(cpf)
 
 DROP TABLE sale
 CREATE TABLE sale(
+customer	char(11) NOT NULL,
 date		TIMESTAMP NOT NULL,
 status		BIT NOT NULL,
 price		DECIMAL(9,2) NOT NULL,
-customer	char(11) NOT NULL,
 card		char(12),
 FOREIGN KEY (customer) REFERENCES customer(cpf),
 FOREIGN KEY (card) REFERENCES card(number),
 PRIMARY KEY (customer, date)
+)
+
+DROP TABLE sale_items
+CREATE TABLE sale_items(
+customer	INT NOT NULL,
+date		TIMESTAMP NOT NULL,
+product		INT NOT NULL,
+quantity	INT NOT NULL,
+total		DECIMAL(9,2),
+FOREIGN KEY (customer) REFERENCES sale(customer),
+FOREIGN KEY (date) REFERENCES sale(date),
+FOREIGN KEY (product) REFERENCES product(id),
+PRIMARY KEY (customer, date, product)
 )
